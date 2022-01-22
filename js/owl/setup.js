590aafa5f599e1712dc1aaad6a3376871336e20e
lgpgomes <gutto-pg@hotmail.com>
2022-01-22 10:13:01 -0300

    algumas alterações

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,18 +1,20 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    margin: 20,
+    navText: ["<i class='fa fa-arrow-left p-2'></i>","<i class='fa fa-arrow-right p-2'></i>"],
+    nav: true,
+    dots: false,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
+        },
+        800:{
+            items:4
         },
         1000:{
             items:5
         }
     }
 })
-
